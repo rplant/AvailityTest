0e642d1aa40d8717e9174d4cc566cf3e97c4bb95
John Plant <rplant@johnplant.net>
2022-07-10 15:11:33 -0400

    Update to CSV

--- cvs_tester/Program.cs
+++ cvs_tester/Program.cs
@@ -14,17 +14,29 @@ namespace csv_tester
             var result = csv.readFile(@"c:\\test.txt");
 
 
-            Console.WriteLine("\n\nSort By Company\n");
-            result.OrderBy(s => s.insurance).ThenByDescending(v => v.version).ToList().ForEach(s =>
-            {
-                Console.WriteLine($"{s.insurance} {s.firstName} {s.lastName} {s.version}");
-            });
+            //Console.WriteLine("\n\nSort By Company\n");
+            //result.OrderBy(s => s.insurance).ThenByDescending(v => v.version).ToList().ForEach(s =>
+            //{
+            //    Console.WriteLine($"{s.insurance} {s.firstName} {s.lastName} {s.version}");
+            //});
 
-            Console.WriteLine("\n\nSort By Last Name, First\n");
-            result.OrderBy(s => s.lastName).ThenBy(s => s.firstName).ToList().ForEach(s =>
+            var groupbyInsurance = from r in result
+                                   group r by r.insurance into InsGroup
+                                   orderby InsGroup.Key
+                                   select InsGroup;
+
+            foreach(var n in groupbyInsurance)
             {
-                Console.WriteLine($"{s.lastName} {s.firstName} {s.insurance} {s.version}");
-            });
+                Console.WriteLine($"Key: {n.Key}");
+                var sorted = n.OrderBy(s => s.lastName).ThenBy(s => s.firstName).ToList();
+                foreach (var i in sorted)
+                {
+                  Console.WriteLine($"{i.lastName} {i.firstName}");
+                }
+            }
+
+
+
 
         }
     }
