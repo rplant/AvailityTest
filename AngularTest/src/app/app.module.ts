d62797f742170c11479397be5b5b0ccb95843156
John Plant <rplant@johnplant.net>
2022-07-10 12:24:57 -0400

    Form Add

--- AngularTest/src/app/app.module.ts
+++ AngularTest/src/app/app.module.ts
@@ -3,12 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
+    FormsModule,
     BrowserModule,
     AppRoutingModule
   ],
