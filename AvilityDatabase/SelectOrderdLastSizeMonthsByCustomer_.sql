1b0e9e0bfcd4ee47a2ce076d1c6d0dc49971c368
John Plant <rplant@johnplant.net>
2022-07-12 11:07:12 -0400

    SP

--- AvilityDatabase/SelectOrderdLastSizeMonthsByCustomer_.sql
+++ AvilityDatabase/SelectOrderdLastSizeMonthsByCustomer_.sql
@@ -1,11 +1,29 @@
 ﻿CREATE PROCEDURE [dbo].[SelectOrderdLastSizeMonthsByCustomer]
-	@customerid int
 AS
-	SELECT  ol.OrdID,
-            sum(ol.Cost * ol.Quantity)
+
+    ;with total_cte as 
+    (
+        select 
+            c.CustId,
+            coalesce(sum(ol.Cost * ol.Quantity),0) as Total 
+        from Customer c
+        left join [Order] o on o.CustomerID = C.CustID
+        left join OrderLine ol on ol.OrdID = o.OrderID
+        where o.OrderDate > dateadd(m, -6, getdate() - datepart(d, getdate()) + 1)
+        group by c.CustID
+        
+    )
+
+
+    SELECT  c.FirstName,
+            c.LastName,
+            coalesce(sum(ol.Cost * ol.Quantity),0) as Total 
     from Customer c
-	left join [Order] o on o.CustomerID = @customerid
+    left join total_cte t on t.CustID = c.CustID
+    left join [Order] o on o.CustomerID = C.CustID
     left join OrderLine ol on ol.OrdID = o.OrderID
-    where o.OrderDate >  dateadd(m, -6, getdate() - datepart(d, getdate()) + 1)
-    group by ol.ordid
+        and t.Total > 100 and t.Total < 500
+    where o.OrderDate > dateadd(m, -6, getdate() - datepart(d, getdate()) + 1)
+        AND Total > 100
+    group by C.LastName, C.FirstName, ol.OrdID, t.Total
 
