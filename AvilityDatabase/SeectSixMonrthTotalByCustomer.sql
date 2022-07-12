8bb394ef717b902e007080b6b187eb03099faa38
John Plant <rplant@johnplant.net>
2022-07-12 10:52:51 -0400

    tweak

--- AvilityDatabase/SeectSixMonrthTotalByCustomer.sql
+++ AvilityDatabase/SeectSixMonrthTotalByCustomer.sql
@@ -1,13 +1,13 @@
 ﻿CREATE PROCEDURE [dbo].[SelectSizeMonthTotalByCustomer]
-	@id int
 AS
 
-
-    SELECT  ol.OrdID,
-            sum(ol.Cost * ol.Quantity)
+    SELECT  c.FirstName,
+            c.LastName,
+            ol.OrdID,
+           coalesce(sum(ol.Cost * ol.Quantity),0) as Total
     from Customer c
-	left join [Order] o on o.CustomerID = @id
+    left join [Order] o on o.CustomerID = C.CustID
     left join OrderLine ol on ol.OrdID = o.OrderID
     where o.OrderDate >  dateadd(m, -6, getdate() - datepart(d, getdate()) + 1)
-    group by ol.ordid
+    group by C.LastName, C.FirstName, ol.OrdID
 
